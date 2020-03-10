ee98cd90662e3a1c02ca8ec195ef638bf462ce75
Antoine-Auffret <antoine@auffret.ovh>
2020-03-10 14:24:59 -0400

    add comments to Reader.cs

--- SudokuSolver/Reader.cs
+++ SudokuSolver/Reader.cs
@@ -8,6 +8,7 @@ namespace SudokuSolver
     {
         private List<string> listSudokuPath = new List<string>();
         private List<string> listSudokuName = new List<string>();
+        // List of sudoku grid
         private List<int[,]> listSudoku2d = new List<int[,]>();
 
         public Reader()
@@ -15,6 +16,7 @@ namespace SudokuSolver
             ListSudoku();
         }
 
+        // Get path and name of all the sudoku .txt files in the sudoku folder
         public void ListSudoku()
         {
             string sudokuDirectory = Environment.CurrentDirectory + "/Sudoku";
@@ -27,27 +29,12 @@ namespace SudokuSolver
             }
         }
 
-        public List<string> getListSudokuPath()
-        {
-            return listSudokuPath;
-        }
-
         public List<string> getListSudokuName()
         {
             return listSudokuName;
         }
 
-        public string getSudokuPath(string path)
-        {
-            return listSudokuPath.Find(element => element.Contains(path));
-        }
-
-        public String getSudoku(string path)
-        {
-            string file = File.ReadAllText(path);
-            return file;
-        }
-
+        // Get the size of a sudoku grid
         public int getSize(string path)
         {
             string[] lines = File.ReadAllLines(path);
@@ -59,22 +46,29 @@ namespace SudokuSolver
             return listSudoku2d;
         }
 
+        // Read the all the sudoku grids from files and put them in the list of matrix listSudoku2d
         public void fillSudoku2d()
         {
             foreach (string path in listSudokuPath)
             {
                 int size = getSize(path);
+                // create a sudo matrix to store the sudoku
                 int[,] sudoku2d = new int[size, size];
+                // read each line of the sudoku in the file
                 string[] lines = File.ReadAllLines(path);
                 for (int i = 0; i < lines.Length; i++)
                 {
+                    // get each line
                     string line = lines[i];
                     for (int j = 0; j < lines[i].Length; j++)
                     {
+                        // get each characters of the line
                         char square = line[j];
+                        // convert char to int and store in the matrix
                         sudoku2d[i, j] = square - '0';
                     }
                 }
+                // add the matrix to the list of sudoku
                 listSudoku2d.Add(sudoku2d);
             }
         }
